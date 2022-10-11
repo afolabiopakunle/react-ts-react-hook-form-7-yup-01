d873894058f24a651fc47793d8b6abe068614c16
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-11 21:07:47 +0100

    yupResolver added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
 import './style.css';
